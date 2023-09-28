a6600138576704665a4b332a3553efdfdfecb8d4
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-09-28 16:16:55 +0700

    make and run function to create graph using a post request

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
-from tokens import TOKEN, USERNAME
+from tokens import TOKEN, USERNAME, GRAPHID
 
 pixela_endpoint = "https://pixe.la/v1/users"
 USERNAME = USERNAME
 TOKEN = TOKEN
+
+header = {
+    "X-USER-TOKEN": TOKEN
+}
 
 user_params = {
     "token": TOKEN,
@@ -12,7 +16,24 @@ user_params = {
     "notMinor": "yes"
 }
 
+graph_config = {
+    "id": GRAPHID,
+    "name": "reading",
+    "unit": "pages",
+    "type": "int",
+    "color": "ajisai"
+
+}
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
 
 def create_new_user():
     response = requests.post(url=pixela_endpoint, json=user_params)
     print(response.text)
+
+def create_graph():
+    response = requests.post(url=graph_endpoint, json=graph_config, headers=header)
+    print(response.text)
+
+create_graph()
